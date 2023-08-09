3a8c9ce0c19f4285ec84545fed39b59d8773196f
Faith-Hall <135525815+Faith-Hall@users.noreply.github.com>
2023-08-09 10:03:33 -0500

    Weather API

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "92cfdd6ed1437da9647f453b414b0f3d"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = ""
